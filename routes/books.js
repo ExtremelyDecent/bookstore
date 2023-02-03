b6c5a3494d40bea28e3361b4cc3cf756365520dc
Henry Frost <henry.90.frost@gmail.com>
2023-02-03 09:18:18 -0500

    testing files

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,9 @@ const express = require("express");
 const Book = require("../models/book");
 
 const router = new express.Router();
+const{validate} = require("jsonschema");
+const bookSchemaNew = require("../schemas/bookSchemaNew");
+const bookSchemaUpdate = require("../schemas/bookSchemaUpdate");
 
 
 /** GET / => {books: [book, ...]}  */
@@ -30,9 +33,17 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const validation = validate(req.body. bookSchemaNew);
+    if (!validation.valid){
+      return (next({
+        status : 400,
+        error : validation.errors.map(e => e.stack)
+      }));
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
-  } catch (err) {
+  } 
+  catch (err) {
     return next(err);
   }
 });
